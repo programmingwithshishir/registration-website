bb5a200f1dacf2ace3e7010449d8d672e4490e6a
Shishir Shetty <programmingwithshishir@gmail.com>
2025-02-25 23:23:08 +0530

    Add 'Event Rules' section

---
+++ src/components/eventrules.jsx
@@ -0,0 +1,129 @@
+const EventRules = () => {
+  return (
+    <div className="font-primary flex flex-col justify-center items-center">
+      <h1 className="mt-8 mb-4 lg:mb-6 text-white font-primary font-bold text-2xl lg:text-4xl md:text-3xl">
+        -Event Rules-
+      </h1>
+      {/* Tech-Know: IT Quiz */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-10 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Tech-Know: <br className="md:hidden" /> IT Quiz
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: 2 <br /> -Number of rounds: 3 <br />{" "}
+          -Questions will be focused on technology <br className="md:hidden" />{" "}
+          and general knowledge. <br /> -No electronic gadgets are allowed.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Varshitha:</span> 8867041668{" "}
+          <br /> <span className="text-blue-400 font-bold">Sahana:</span>{" "}
+          8197235527
+        </h1>
+      </div>
+      {/* Pixel-Craft: Web Designing */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-10 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Pixel-Craft: <br className="md:hidden" /> Web Designing
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: 2 <br /> -Number of rounds: 3 <br />{" "}
+          -Knowledge of HTML, CSS and Javascript is <br className="md:hidden" />{" "}
+          necessary. <br /> -No electronic gadgets are allowed.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Suman:</span> 6361208857{" "}
+          <br /> <span className="text-blue-400 font-bold">Puneeth:</span>{" "}
+          8088076080
+        </h1>
+      </div>
+      {/* Byte-Battle: Coding */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-10 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Byte-Battle: <br className="md:hidden" /> Coding
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: 1 <br /> -Number of rounds: 3 <br />{" "}
+          -Participants are free to use any programming language of their
+          choice. <br /> -No electronic gadgets are allowed.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Shishir:</span> 8108452426{" "}
+          <br /> <span className="text-blue-400 font-bold">Jessica:</span>{" "}
+          9741132992
+        </h1>
+      </div>
+      {/* Tech-Tix: IT Manager */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-10 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Tech-Tix: <br className="md:hidden" /> IT Manager
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: 1 <br /> -Number of rounds: 3 <br />{" "}
+          -Participants must carry their updated resume. <br /> -No electronic
+          gadgets are allowed.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Numa:</span> 7760453135{" "}
+          <br /> <span className="text-blue-400 font-bold">Rashmi:</span>{" "}
+          9483538319
+        </h1>
+      </div>
+      {/* Tech-Reveal: Product Launch */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-10 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Tech-Reveal: <br className="md:hidden" /> Product Launch
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: 3 <br /> -Number of rounds: 1 <br />{" "}
+          -Participants must carry their own laptops.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Pranamya:</span> 8618288097{" "}
+          <br /> <span className="text-blue-400 font-bold">Dwithi:</span>{" "}
+          8861852637
+        </h1>
+      </div>
+      {/* Cine-Scope: Videography */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-10 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Cine-Scope: <br className="md:hidden" /> Videography
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: 1 <br /> -Number of rounds: 3 <br />{" "}
+          -Participants must carry their own equipments. <br /> No stock or
+          precaptured videos should be used.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Shazil:</span> 9880123647{" "}
+          <br /> <span className="text-blue-400 font-bold">Sarfaraz:</span>{" "}
+          7795848680
+        </h1>
+      </div>
+      {/* Rhythm: Cultural Dance */}
+      <div className="pl-8 md:pl-32 lg:pl-52 pb-20 w-full">
+        <h1 className="text-blue-400 text-2xl font-bold">
+          Rhythm: <br className="md:hidden" /> Cultural Dance
+        </h1>
+        <p className="text-white text-sm">
+          -Number of participants: Minimum 8 - Maximum 10 <br /> -Time
+          constraints must strictly be followed (5+1 minutes) <br /> -Theme of
+          the dance must stricly be Folk. <br /> -Props can be used.
+        </p>
+        <h1 className="text-white">
+          Event Coordinators: <br />{" "}
+          <span className="text-blue-400 font-bold">Samiksha:</span> 8010156146{" "}
+          <br /> <span className="text-blue-400 font-bold">Navya:</span>{" "}
+          8123700233
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default EventRules;
